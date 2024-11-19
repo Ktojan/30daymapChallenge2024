7fcc939ab263d0737b044593b69f88ebe51ed8dc
Andrey <karpovich.andrey@gmail.com>
2024-11-19 13:46:24 +0200

    new modules and routings

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,12 +4,13 @@ import { ProjectAccessGuard } from './project-access.guard';
 import { Day183DComponent } from './day18-3d/day18.component';
 import { NotFoundPageComponent } from './not-found-page/not-found-page.component';
 import { Day20Component } from './day20-osm/day20.component';
+import { Day24Component } from './day24-circular/day24.component';
 
 const routes: Routes = [
   {path: 'day18', component: Day183DComponent },
-  {path: 'day20', component: Day20Component, canActivate: [ProjectAccessGuard] },
+  {path: 'day24', component: Day24Component },
   {path: 'day26', component: Day183DComponent, canActivate: [ProjectAccessGuard] },
-  {path: '', pathMatch: 'full', redirectTo: 'day18'},
+  {path: '', pathMatch: 'full', redirectTo: 'day24'},
   {path: '**', component: NotFoundPageComponent}
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,31 +6,36 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { Day183DComponent } from './day18-3d/day18.component';
 import { Day20Component } from './day20-osm/day20.component';
+import { Day24Component } from './day24-circular/day24.component';
 // --------------------------- MAPBOX stuff and files ----------------- //
 import { NgxMapboxGLModule } from 'ngx-mapbox-gl';
 // ---------------------------Material stuff ----------------------//
 import { MatButtonModule } from '@angular/material/button';
 import { MatSlideToggleModule } from '@angular/material/slide-toggle';
-import { MatDialogModule } from '@angular/material/dialog';
 import { MapboxService } from './day18-3d/mapbox.service';
 import { MatCardModule } from '@angular/material/card';
 import { NotFoundPageComponent } from './not-found-page/not-found-page.component';
 import { MatIconModule } from '@angular/material/icon';
 import { MatMenuModule } from '@angular/material/menu';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
+import { MatDialogModule,  MatDialogActions,  MatDialogClose,  MatDialogContent, MatDialogTitle } from '@angular/material/dialog';
 
 @NgModule({
   declarations: [
     AppComponent,
     Day183DComponent,
     Day20Component,
+    Day24Component,
     NotFoundPageComponent
   ],
   imports: [
     BrowserModule, BrowserAnimationsModule,
     AppRoutingModule,
     FormsModule,
-    MatDialogModule, MatCardModule, MatButtonModule, MatSlideToggleModule, MatIconModule, MatMenuModule,
+    MatDialogModule, MatDialogActions,  MatDialogClose,  MatDialogContent, MatDialogTitle , MatCardModule, MatButtonModule, MatSelectModule,
+    MatInputModule, MatSlideToggleModule, MatIconModule, MatMenuModule,
     NgxMapboxGLModule,
   ],
   providers: [MapboxService],
