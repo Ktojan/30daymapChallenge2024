a429a5247d770a6a8551157b623ad32965f9afe9
Andrey <karpovich.andrey@gmail.com>
2024-11-19 13:46:56 +0200

    update to app comp

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component, inject, TemplateRef, ViewChild } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
+import { NavigationEnd, Router } from '@angular/router';
+import { filter, map } from 'rxjs';
 
 @Component({
   selector: 'comp-root',
@@ -9,26 +11,42 @@ import { MatDialog } from '@angular/material/dialog';
 export class AppComponent {
   @ViewChild('aboutModal') aboutModal: TemplateRef<HTMLElement>;
   dialog = inject(MatDialog);
+  selectedDayLabel = '';
+  displayAbout = true;
+  private router = inject(Router);
 
   menuLinks = [
     {
       routerLink: '/day18',
       icon: 'view_in_ar',
-      label: 'Day 18: 3D',      
+      label: 'Day 18 - 3D',
+      hasOwnAbout: false      
     },
     {
-      routerLink: '/day20',
-      icon: 'volunteer_activism',
-      label: 'Day 20: OSM',
-      disabled: true
-    },
+      routerLink: '/day24',
+      icon: 'grid_4x4',
+      label: 'Day 24 - Circular',
+      hasOwnAbout: true     
+    },    
     {
       routerLink: '/day26',
       icon: 'grid_4x4',
-      label: 'Day 26: Projections',
+      label: 'Day 26 - Projections',
       disabled: true
     }    
   ]
+
+  ngOnInit() {
+    this.router.events.pipe(
+      filter((event: any) => event instanceof NavigationEnd),
+      map((event: NavigationEnd) => event.url)
+    ).subscribe(link => {
+      console.log(link);
+      const item = this.menuLinks.find(el => el.routerLink === link);
+      this.selectedDayLabel = item?.label || '';
+      this.displayAbout = !item.hasOwnAbout;
+  })
+  }
 
   clickAbout(enterAnimationDuration: string, exitAnimationDuration: string) {
     this.dialog.open(this.aboutModal, { 
